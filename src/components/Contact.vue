4c3a7d91f0e203900bcb0eaad0296ba513c837fd
vantablanta <njerimwanjiku@gmail.com>
2021-02-23 11:31:57 +0300

    Add responsiveness to the contact page

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,8 +3,13 @@
         <div class="topnave">
             <a href=""><i class="fab fa-github"></i></a>
             <a href=""><i class="fab fa-twitter"></i></a>
-            <a href=""><i class="fab fa-free-code-camp"></i></a>
-
+            <a href=""><i class="fab fa-free-code-camp" style="font-weight: bolder;"></i></a>
+        
+          <p v-for="{contactInfo, index } in contactInfos" v-bind:key="index">
+          {{contactInfo}}
+          <img v-if="index === 1">
+          </p>
+        
         </div>
     </div>
 </template>
@@ -13,9 +18,38 @@
 
 export default {
     name: 'Contact',
+    data(){
+        return{
+            contactInfos: [
+                    {'contactInfo': 'To get intouch for work or consultation you can reach me through'},
+                    {'contactInfo': 'njerimwanjiku@gmail.com'},
+                    {'contactInfo': '+254-796693270'},
+            ],
+           
+        }
+    },
+    
+    
 }
 </script>
 
 <style scoped>
+.topnave{
+    box-sizing: border-box ;
+    display: block;
+    padding: 10px 5px;
+    text-align: center;
+    justify-content: center;
+
+}
+a{
+    list-style: none;
+    text-decoration: none;
+    padding: 5px 10px;
+    color: black;
+   
+}
+
+
 
 </style>
